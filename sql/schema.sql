c11e2ef0784a313b4a81ef25273d01e84eea5ab6
Sam Meech-Ward <sam@meech-ward.com>
2019-10-07 08:54:29 -0700

    Add schema and seeds
    For oscar award data http://awardsdatabase.oscars.org/

---
+++ sql/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS awards;
+DROP TABLE IF EXISTS actors;
+
+CREATE TABLE awards (
+  id INTEGER PRIMARY KEY,
+  year CHAR(4),
+  title VARCHAR(255),
+  winner_id INTEGER
+);
+
+CREATE TABLE actors (
+  id INTEGER PRIMARY KEY,
+  full_name VARCHAR(255) unique
+);
