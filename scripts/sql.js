3e57ab95628ff8af39fd6def65c9fef02913ff8e
Sam Meech-Ward <sam@meech-ward.com>
2019-10-10 15:04:57 -0700

    Fix full to left" ;

--- scripts/sql.js
+++ scripts/sql.js
@@ -10,7 +10,7 @@ function rightToLeft(query) {
 }
 
 function fullToLeft(query) {
-  const regex = /(select[\n\s\w\d,.]*)from[\s\n]+([a-zA-Z0-9_]+)[\s\n]*full[\s\n]+(outer )?[\s\n]*join[\s\n]+([a-zA-Z0-9_]+)[\s\n]+on((?:(?!order)[\w\d\s\n=.])*)(order)?([\w\d\s\n.=]*)(;*)/i
+  const regex = /(select[\n\s\w\d,.*()]*)from[\s\n]+([a-zA-Z0-9_]+)[\s\n]*full[\s\n]+(outer )?[\s\n]*join[\s\n]+([a-zA-Z0-9_]+)[\s\n]+on((?:(?!order)[\w\d\s\n=.])*)(order)?([\w\d\s\n.=]*)(;*)/i
   return query.replace(regex, function(match, p1, p2, p3, p4, p5, p6, p7, p8) {
     return `${p1}from ${p2} left join ${p4} on${p5} union ${p1}from ${p4} left join ${p2} on${p5} ${p6 || ''} ${p7 || ''} ${p8 || ''}`;
   });
