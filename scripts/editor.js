c313e9f4b4a01925fab63da20f6d155c57bb956e
Sam Meech-Ward <sam@meech-ward.com>
2019-10-10 11:25:26 -0700

    Use multiple editors

--- scripts/editor.js
+++ scripts/editor.js
@@ -1,28 +1,35 @@
-const $editors = $('.editor');
-editors = [];
-$editors.each((index, editorElement) => {
-  // pass options to ace.edit
-  const editor = ace.edit(editorElement, {
-    mode: "ace/mode/sql",
-    selectionStyle: "text"
-  })
-  editors.push(editor);
-  // use setOptions method to set several options at once
-  editor.setOptions({
-    autoScrollEditorIntoView: true,
-    copyWithEmptySelection: true,
-  });
-  // use setOptions method
-  editor.setOption("mergeUndoDeltas", "always");
+$(() => {
+  const $editorComponents = $('.sql-editor-component');
+  window.editors = {};
 
-  // some options are also available as methods e.g. 
-  editor.setTheme("ace/theme/twilight");
+  $editorComponents.each((index, editorComponent) => {
+    const $editorComponent = $(editorComponent);
+    const editorName = $editorComponent.data('editor');
 
-  const key = `editor-${index}-code`;
-  const code = localStorage.getItem(key);
-  editor.setValue(code || "// Write your code here");
+    // pass options to ace.edit
+    const editor = ace.edit($editorComponent.find('.editor')[0], {
+      mode: "ace/mode/sql",
+      selectionStyle: "text"
+    })
+    editors[editorName] = editor;
 
-  editor.session.on('change', function(delta) {
-    localStorage.setItem(key, editor.getValue());
+    // use setOptions method to set several options at once
+    editor.setOptions({
+      autoScrollEditorIntoView: true,
+      copyWithEmptySelection: true,
+    });
+    // use setOptions method
+    editor.setOption("mergeUndoDeltas", "always");
+
+    // some options are also available as methods e.g. 
+    editor.setTheme("ace/theme/twilight");
+
+    const key = `editor-${index}-code`;
+    const code = localStorage.getItem(key);
+    editor.setValue(code || "// Write your code here");
+
+    editor.session.on('change', function(delta) {
+      localStorage.setItem(key, editor.getValue());
+    });
   });
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,70 @@
+const getSchemeAndSeeds = () => Promise.all([
+  $.get('/sql/schema.sql'),
+  $.get('/sql/seeds.sql')
+]);
+
+function tableFromOutput(output) {
+  const addTags = R.curry((open, close, string) => `${open}${string}${close}`);
+  const addTH = addTags('<th>', '</th>');
+  const addTD = addTags('<td>', '</td>');
+  const addTR = addTags('<tr>', '</tr>');
+  const joiner = R.join('');
+
+  const tableRow = R.pipe(R.map, joiner, addTR);
+  const tableRows = R.pipe(R.map(R.pipe(R.map(addTD), joiner, addTR)), joiner);
+  // const trs = output[0].values.map(col => `<tr>``<td>${col}</td>`).join("");
+  return `
+    <table>
+        <thead>
+          ${tableRow(addTH, output[0].columns)}
+        </thead>
+        <tbody>
+          ${tableRows(output[0].values)}
+        </tbody>
+    </table>
+    `;
+}
+
+function executeButtonClicked(event) {
+  event.preventDefault();
+  const $editorComponent = $(event.target).closest('.sql-editor-component');
+  const editorName = $editorComponent.data('editor');
+
+  let sql = editors[editorName].getValue();
+
+  if (containsRightJoin(sql)) {
+    sql = rightToLeft(sql);
+  }
+  
+  const output = db.exec(sql);
+  console.log(output);
+
+  let html;
+
+  if (!output[0]) {
+    html = '<p>👍</p>';
+  } else {
+    html = `<h5>Total Rows: ${output[0].values.length}</h5>` + tableFromOutput(output);
+  }
+
+  $editorComponent.find(".output")
+    .empty()
+    .append(html);
+}
+
 $(async () => {
-  const [schema, seeds, SQL] = await Promise.all([
-    $.get('http://thesqljoins.com/sql/schema.sql'),
-    $.get('http://thesqljoins.com/sql/seeds.sql'),
-    initSqlJs({ locateFile: filename => `/sqljs/${filename}` })
-  ]);
-  const db = new SQL.Database();
-  db.run(schema);
-  db.run(seeds);
-  editors[0].setValue (schema+"\n"+seeds);
-  editors[1].setValue(`SELECT year, title, full_name 
+  const SQL = await initSqlJs({ locateFile: filename => `/sqljs/${filename}` });
+  window.db = new SQL.Database();
+
+  const [schema, seeds] = await getSchemeAndSeeds();
+
+  editors['schema'].setValue(schema);
+  editors['seeds'].setValue(seeds);
+
+  editors['joins'].setValue(`SELECT year, title, full_name 
 FROM actors 
-JOIN awards on actors.id = awards.winner_id;`);
+JOIN awards on actors.id = awards.winner_id
+ORDER BY year DESC;`);
 
-  $("#execute-button").on("click", function(event) {
-    event.preventDefault();
-    let sql = editors[1].getValue();
-    if (containsRightJoin(sql)) {
-      sql = rightToLeft(sql);
-    }
-    
-    const output = db.exec(sql);
-    console.log(output);
-
-    const addTags = R.curry((open, close, string) => `${open}${string}${close}`);
-    const addTH = addTags('<th>', '</th>');
-    const addTD = addTags('<td>', '</td>');
-    const addTR = addTags('<tr>', '</tr>');
-    const joiner = R.join('');
-
-    const tableRow = R.pipe(R.map, joiner, addTR);
-    const tableRows = R.pipe(R.map(R.pipe(R.map(addTD), joiner, addTR)), joiner);
-    // const trs = output[0].values.map(col => `<tr>``<td>${col}</td>`).join("");
-    $("#output")
-      .empty()
-      .append(
-      $(`
-      <table>
-          <thead>
-            ${tableRow(addTH, output[0].columns)}
-          </thead>
-          <tbody>
-            ${tableRows(output[0].values)}
-          </tbody>
-      </table>
-      `)
-      );
-    
-  })
+  $(".execute-button").on("click", executeButtonClicked);
 });
